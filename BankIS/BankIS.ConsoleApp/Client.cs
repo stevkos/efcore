08b600a5077cfb86023f6276903f2af849c829f8
stevkos <91120583+stevkos@users.noreply.github.com>
2021-09-21 12:09:13 +0200

    First app init
    First app

---
+++ BankIS/BankIS.ConsoleApp/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankIS.ConsoleApp
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public void Print ()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+}
---
+++ BankIS/BankIS.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace BankIS.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Client c1 = new Client();
+            c1.Name = "Jan Novák";
+            c1.Age = 31;
+            c1.Print();
+        }
+    }
+}
